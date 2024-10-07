1c7462bbf93821ec9f13689d765a6c523ee28921
Trinity-Kerr <135732091+Trinity-Kerr@users.noreply.github.com>
2024-10-07 16:31:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,34 @@
+function skillsMember() {
+  return [
+    {
+      name: "HTML",
+      icon: "fab fa-html5",
+      level: "80",
+    },
+    {
+      name: "CSS",
+      icon: "fab fa-css3-alt",
+      level: "70",
+    },
+    {
+      name: "JavaScript",
+      icon: "fab fa-js-square",
+      level: "60",
+    },
+    {
+      name: "React",
+      icon: "fab fa-react",
+      level: "50",
+    },
+    {
+      name: "NodeJS",
+      icon: "fab fa-node-js",
+      level: "40",
+    },
+    {
+      name: "MongoDB",
+      icon: "fas fa-database",
+      level: "40",
+    },
+  ];
+}
